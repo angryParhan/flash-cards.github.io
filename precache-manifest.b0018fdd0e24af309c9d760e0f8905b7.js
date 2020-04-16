35b86c96dfcbb2793c5454bd9db1dbfc59c271bc
Andriy <yuraparhomchuk@gmail.com>
2020-04-16 17:29:56 +0300

    leviOsa

---
+++ precache-manifest.b0018fdd0e24af309c9d760e0f8905b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2bba44f9b06f4eafe1",
+    "url": "css/about.dc7cd5fc.css"
+  },
+  {
+    "revision": "4951ce58eef2e678a9d0",
+    "url": "css/app.7c0a5412.css"
+  },
+  {
+    "revision": "af356ccdd57cedc246d7",
+    "url": "css/chunk-vendors.24bf1742.css"
+  },
+  {
+    "revision": "e0f7b4e7060540cc297c88788808f7b5",
+    "url": "index.html"
+  },
+  {
+    "revision": "2b2bba44f9b06f4eafe1",
+    "url": "js/about.4e5a5694.js"
+  },
+  {
+    "revision": "4951ce58eef2e678a9d0",
+    "url": "js/app.0d372e68.js"
+  },
+  {
+    "revision": "af356ccdd57cedc246d7",
+    "url": "js/chunk-vendors.8a1f6971.js"
+  },
+  {
+    "revision": "ccba393e98f04a0ddb064e00de282ef9",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
